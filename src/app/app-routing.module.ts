76502b1ab254780618e0b9aed61f4103763da38b
javier <javiercangahualamarquez@gmail.com>
2023-01-31 12:43:46 -0500

    terminando tarea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MisionComponent } from './pages/mision/mision.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { VisionComponent } from './pages/vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'nosotros', component:NosotrosComponent,
+  children: [
+    {path: 'mision', component:MisionComponent},
+    {path: 'vision', component:VisionComponent}
+  ]},
+  {path:'contacto', component:ContactoComponent},
+  {path: '**', redirectTo:'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
